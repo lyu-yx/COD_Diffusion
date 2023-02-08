272db338d0634b39d9086c0542de5b266a70bc6f
yx-lyu <lvyixuan@njust.edu.cn>
2023-02-08 10:48:27 +0800

    debug

--- scripts/segmentation_sample.py
+++ scripts/segmentation_sample.py
@@ -112,7 +112,7 @@ def main():
             output = F.interpolate(sample, size=img_size, mode='bilinear', align_corners=False)
             output = output.squeeze().cpu().numpy()
             output = (output - output.min()) / (output.max() - output.min() + 1e-8)
-            plt.imsave('./results/' + str(name).split('.')[0] + '.png', output, cmap='gist_gray') # save the generated mask
+            plt.imsave('./results/val/' + str(name).split('.')[0] + '.png', output, cmap='gist_gray') # save the generated mask
             
             # s = th.tensor(sample).squeeze().cpu().numpy()
             # plt.imsave('./results/' + str(name).split('.')[0] + '.png', s, cmap='gist_gray') # save the generated mask
