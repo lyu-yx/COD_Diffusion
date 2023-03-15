19187479da94c7502ca9796538d5a900b77c39c4
yx-lyu <lvyixuan@njust.edu.cn>
2023-03-15 14:29:25 +0800

    DEBUG

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -264,8 +264,8 @@ class GaussianDiffusion:
         B, C = x.shape[:2]
         C=1
         assert t.shape == (B,)
-        model_output, *edge_output = model(x, self._scale_timesteps(t), **model_kwargs)
-        edge_output = edge_output[-1]
+        model_output = model(x, self._scale_timesteps(t), **model_kwargs)
+        edge_output = model_output[-1]
         x=x[:,-1:,...]  #loss is only calculated on the last channel, not on the input brain MR image
         if self.model_var_type in [ModelVarType.LEARNED, ModelVarType.LEARNED_RANGE]:
             assert model_output.shape == (B, C * 2, *x.shape[2:])
