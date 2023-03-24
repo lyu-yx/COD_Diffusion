32c5833cf76754ba7b6a30fa1ba5fb6c166740c6
lyu-yx <lvyixuan@njust.edu.cn>
2023-03-24 16:35:44 +0800

    debug

--- guided_diffusion/unet_ablation.py
+++ guided_diffusion/unet_ablation.py
@@ -1161,6 +1161,6 @@ class IntegratedUNetModel_NoPGFR(nn.Module):
         
         out = self.out(h)
         
-        return out, (th.zeros(1), th.zeros(1), th.zeros(1), th.zeros(1))
+        return out, (th.zeros(1).to ("cuda"), th.zeros(1).to ("cuda"), th.zeros(1).to ("cuda"), th.zeros(1).to ("cuda"))
 
 
